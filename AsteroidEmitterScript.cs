e561fd66c989c0f0c380562d2d78235171b78955
Lucy <47707901+LucyEv@users.noreply.github.com>
2020-04-08 22:27:44 +0300

    Update AsteroidEmitterScript.cs

--- AsteroidEmitterScript.cs
+++ AsteroidEmitterScript.cs
@@ -26,7 +26,7 @@ public class AsteroidEmitterScript : MonoBehaviour
 
             Vector3 newPosition = new Vector3(xPosition, yPosition, zPosition);
 
-            Instantiate(asteroid, newPosition, Quaternion.identity); // функция Instantiate создает астероид, в новой позиции, с пустым вращением
+            Instantiate(asteroid, newPosition, Quaternion.identity); // функция Instantiate создает астероид в новой позиции, с пустым вращением
 
             nextSpawn = Time.time + Random.Range(minDelay, maxDelay);
         }

062c1bbfb9eaf9d40ca099433dcbaa2e89cff0a1
Lucy <47707901+LucyEv@users.noreply.github.com>
2020-05-18 22:12:22 +0300

    Update Scroller.cs

--- Scroller.cs
+++ Scroller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Scroller : MonoBehaviour
 {
-    public float speed; // параметр скорости можно менять снаружи, настраивая скорость движения космического фона
+    public float speed; // параметр скорости можно изменять снаружи, настраивая скорость движения космического фона
     private Vector3 startPosition; 
 
     private void Start()
@@ -14,7 +14,7 @@ public class Scroller : MonoBehaviour
      // Update is called once per frame - на каждый кадр
     void Update()
     {
-        float newPosition = Mathf.Repeat(Time.time * speed, transform.localScale.y); // функция Mathf.Repeat позволяет менять значение переменной (1-новая позиция, 2-граница) 0,1,2,.. 99,100 - 0,1 99
+        float newPosition = Mathf.Repeat(Time.time * speed, transform.localScale.y); // функция Mathf.Repeat позволяет менять значение переменной (1-новая позиция, 2-граница) 0,1,2,..99,100 - 0,1..99
         transform.position = startPosition + Vector3.back * newPosition;
     }
 }

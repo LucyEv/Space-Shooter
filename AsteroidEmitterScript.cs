0aa70f5f74ce3e6c68708cf8dba675400c094bcc
Lucy <47707901+LucyEv@users.noreply.github.com>
2020-05-25 20:59:22 +0300

    Update AsteroidEmitterScript.cs

--- AsteroidEmitterScript.cs
+++ AsteroidEmitterScript.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class AsteroidEmitterScript : MonoBehaviour
 {
     public GameObject asteroid;
-    public float minDelay, maxDelay; // задержка между вылетами астероидов
+    public float minDelay, maxDelay; // задержка между вылетами астероидов / delay between Asteroids' exits
 
-    private float nextSpawn; // время следующего создания астероида
+    private float nextSpawn; // время следующего создания астероида / time for creation next Asteroid
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +17,11 @@ public class AsteroidEmitterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Time.time > nextSpawn) // Если пришло время для создания нового астероида
+      if (Time.time > nextSpawn) // если пришло время для создания нового астероида / if it's time to create new Asteroid
         {
             float yPosition = transform.position.y;
             float zPosition = transform.position.z;
-            float xPosition = Random.Range(-transform.localScale.x/2, transform.localScale.x/2); // влево -половина, вправо +половина
+            float xPosition = Random.Range(-transform.localScale.x/2, transform.localScale.x/2); // влево -половина, вправо +половина / left minus half, right plus half
 
             Vector3 newPosition = new Vector3(xPosition, yPosition, zPosition);
 

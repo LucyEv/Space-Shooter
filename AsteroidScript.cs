1b8cedc491a481b2238ca9ba125a1599018ef691
Lucy <47707901+LucyEv@users.noreply.github.com>
2020-07-21 23:19:00 +0300

    Update AsteroidScript.cs

--- AsteroidScript.cs
+++ AsteroidScript.cs
@@ -15,11 +15,12 @@ public class AsteroidScript : MonoBehaviour
     {
         Asteroid = GetComponent<Rigidbody>();
         Asteroid.angularVelocity = Random.insideUnitSphere * rotationSpeed; // angularVelocity - угловая скорость, как пример = new Vector3(50, 0, 0);
-                                                                            // angularVelocity - for example = new Vector3 (50, 0, 0);
+                                                                            // angularVelocity, for example = new Vector3 (50, 0, 0);
         Asteroid.velocity = Vector3.back * Random.Range(minSpeed, maxSpeed);
     }
     // срабатывает при столкновении с другим коллайдером;
     // triggered by collision with another collider;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "GameBoundary" || other.tag == "Asteroid")
@@ -27,14 +28,15 @@ public class AsteroidScript : MonoBehaviour
             return;
         }
 
-        Instantiate(asteroidExplosion, transform.position, Quaternion.identity); // первый аргумент - что создать (взрыв), второй аргумент - где создать, третье - вращение (нулевое)
+        Instantiate(asteroidExplosion, transform.position, Quaternion.identity); // первый аргумент - что создать (взрыв), второй аргумент - где создать, третье - вращение (нулевое);
                                                                                 // the first argument is what to create (explosion), the second argument is where to create, the third is rotation (zero)
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, Quaternion.identity);
         }
 
-        Destroy(other.gameObject); // уничтожаем объект, с которым столкнулся астероид / destroy the object that asteroid collided with
+        Destroy(other.gameObject); // уничтожаем объект, с которым столкнулся астероид
+                                   // destroy the object that asteroid collided with
         Destroy(gameObject); // уничтожаем сам астероид / destroy the asteroid itself
     }
 

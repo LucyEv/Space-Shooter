acad7d49760cdecdb981e5ae0a43986920dc6cdf
Lucy <47707901+LucyEv@users.noreply.github.com>
2020-12-21 22:35:48 +0300

    Update Scroller.cs

--- Scroller.cs
+++ Scroller.cs
@@ -13,11 +13,11 @@ public class Scroller : MonoBehaviour
         startPosition = transform.position;
     }
      // Update is called once per frame 
-    // обновление на каждый кадр
+     // обновление на каждый кадр
     void Update()
     {
         float newPosition = Mathf.Repeat(Time.time * speed, transform.localScale.y); // функция Mathf.Repeat позволяет менять значение переменной (1-новая позиция, 2-граница) 0,1,2,..99,100 - 0,1..99 /
-                                                                                    // Mathf.Repeat allows to change the value of a variable (1-new position, 2-border)
+                                                                                     // Mathf.Repeat allows to change the value of a variable (1-new position, 2-border)
         transform.position = startPosition + Vector3.back * newPosition;
     }
 }

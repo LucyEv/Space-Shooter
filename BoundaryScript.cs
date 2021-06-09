015059e059b0151db91f9a61d3902f3adad82d32
Lucy <47707901+LucyEv@users.noreply.github.com>
2021-06-09 22:05:26 +0300

    Update BoundaryScript.cs

--- BoundaryScript.cs
+++ BoundaryScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoundaryScript : MonoBehaviour
 {
-    private void OnTriggerExit(Collider other) // срабатывает, когда другой коллайдер покидает (Exit) границы текущего коллайдера 
+    private void OnTriggerExit(Collider other) // Срабатывает, когда другой коллайдер покидает (Exit) границы текущего коллайдера 
                                                // It starts and works when another Collider leaves (Exit) the borders of the current Collider
     {
         Destroy(other.gameObject);
